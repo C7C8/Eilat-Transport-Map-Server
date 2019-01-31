c7c87c3fbad2393f87e301608ad30ca186744c98
Evan Goldstein <evan@icarusnet.me>
2019-01-31 12:34:10 +0200

    get status from XML response

--- Request.py
+++ Request.py
@@ -1,13 +1,29 @@
 import time
 from datetime import datetime
-from xml.etree.ElementPath import findtext
-from xml.etree.ElementTree import Element, SubElement, dump, ElementTree, fromstring
+from xml.etree.ElementTree import Element, SubElement, dump, ElementTree, fromstring, tostring
 import requests
+
+regex = '(?<=<)\\w+:'
 
 mot_siri = 'http://siri.motrealtime.co.il:8081/Siri/SiriServices'
 icarus_siri_proxy = 'http://siri.motrealtime.icarusnet.me/Siri/SiriServices'
 
 url = icarus_siri_proxy
+ns0 = 'http://schemas.xmlsoap.org/soap/envelope/'
+ns1 = 'http://new.webservice.namespace'
+ns3 = 'http://www.ifopt.org.uk/acsb'
+ns4 = 'http://www.ifopt.org.uk/ifopt'
+ns_siri = 'http://www.siri.org.uk/siri'
+ns_siriWS = 'http://new.webservice.namespace'
+nsS = 'http://schemas.xmlsoap.org/soap/envelope/'
+prefix = '<?xml version="1.0" ?>'
+
+
+def xp_ns(tags):
+    path = './'
+    for ns, tag in tags:
+        path += ('{' + ns + '}' if ns is not None else '') + tag + '/'
+    return path[:-1]
 
 
 def siri_tag(name):
@@ -41,7 +57,6 @@ class Node(Element):
             self.add_child(tag, value)
 
     def tostring(self):
-        from xml.etree.ElementTree import tostring
         return tostring(self, encoding='unicode', method='xml')
 
 
@@ -53,6 +68,11 @@ class SIRI_Request:
     xml_siri_request_nodes = []
     xml_response_root = None
     response_code = None
+    __service = None
+
+    def __init__(self, get_service):
+        self.__service = get_service
+        self.build_xml()
 
     def add_timestamp(self, element):
         self.__timestamps.append(SubElement(element, siri_tag('RequestTimestamp')))
@@ -62,18 +82,12 @@ class SIRI_Request:
         for timestamp in self.__timestamps:
             timestamp.text = timestamp_str
 
-    def build_xml(self, service):
+    def build_xml(self):
         self.xml_root_node = Node('S:Envelope')
-        self.xml_root_node.set('xmlns:S', 'http://schemas.xmlsoap.org/soap/envelope/')
+        self.xml_root_node.set('xmlns:S', nsS)
         body = self.xml_root_node.add_child('S:Body')
-        siri_ws = body.add_child('siriWS:' + service)
-        siri_ws.set({
-            'xmlns:siriWS': 'http://new.webservice.namespace',
-            'xmlns': '',
-            'xmlns:ns4': 'http://www.ifopt.org.uk/ifopt',
-            'xmlns:ns3': 'http://www.ifopt.org.uk/acsb',
-            'xmlns:siri': 'http://www.siri.org.uk/siri'
-        })
+        siri_ws = body.add_child('siriWS:' + self.__service)
+        siri_ws.set({'xmlns:siriWS': ns_siriWS, 'xmlns': '', 'xmlns:ns4': ns4, 'xmlns:ns3': ns3, 'xmlns:siri': ns_siri})
         request = siri_ws.add_child('Request')
         self.add_timestamp(request)
         requestor_ref = SubElement(request, 'RequestorRef')
@@ -99,7 +113,7 @@ class SIRI_Request:
     def __send(self):
         self.set_time()
         print('<?xml version="1.0" ?>' + self.xml_root_node.tostring())
-        response = requests.post(url, data='<?xml version="1.0" ?>' + self.xml_root_node.tostring(),
+        response = requests.post(url, data=prefix + self.xml_root_node.tostring(),
                                  headers={'Content-Type': 'text/xml'})
         return response.status_code, response.text
 
@@ -107,21 +121,25 @@ class SIRI_Request:
         data = self.__send()
         self.response_code = data[0]
         self.xml_response_root = fromstring(data[1])
-        dump(self.xml_response_root)
-        # answer = findtext(self.xml_response_root, 'Answer')
-        # dump(answer)
-        # status = findtext(answer, 'Status')
-        # dump(status)
+        path = xp_ns([(ns0, 'Body'), (ns1, self.__service + 'Response'), (None, 'Answer')])
+        answer = self.xml_response_root.findall(path)[0]
+        if answer is None:
+            print("Error: NoneType for ElementTree")
+            return
+        # print('ANSWER:', tostring(answer))
+        path = xp_ns([(ns_siri, 'Status')])
+        status = answer.findall(path)[0].text
+        if status == 'true':
+            print("Request was successful!")
 
 
 class StopMonitoringRequest(SIRI_Request):
     def __init__(self, preview_interval, ref, max_visits='100', start_time=get_time()):
-        super().__init__()
+        super().__init__('GetStopMonitoringService')
         self.preview_interval = preview_interval
         self.ref = ref
         self.start_time = start_time
         self.max_visits = max_visits
-        super().build_xml('GetStopMonitoringService')
         smr = self.addRequest('StopMonitoringRequest')
         smr.add_children({
             'PreviewInterval': self.preview_interval,
@@ -135,9 +153,7 @@ class StopMonitoringRequest(SIRI_Request):
 if __name__ == '__main__':
     req = StopMonitoringRequest('PT30M', '32902')
     req.set_time()
-    req.response()
-    # data = req.__send()[1]
-    # dump(dataTree)
-    # print(data)
-    # req.send()
-    # dump(req.xml_root)
+    try:
+        req.response()
+    except requests.exceptions.ConnectionError as e:
+        print("Failed to connect to " + url)
